6eac97fd286391796030c2229b7cb472cc146e5a
Jesse Ojih <jcinvent05@gmail.com>
2022-04-03 09:43:13 +0100

    Done!

--- key-store.js
+++ key-store.js
@@ -1,11 +1,22 @@
-const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
-const fs = require('fs');
-const shortid = require('shortid');
+const VALID_KEYS_PATH = __dirname + "/valid-keys.txt";
+const fs = require("fs");
+const shortid = require("shortid");
 // To generate a unique API KEY, use shortid.generate()
-const LINE_ENDING = require('os').EOL;
-
+const LINE_ENDING = require("os").EOL;
 
 module.exports = function (req, res) {
+  /**
+   * Generating api key
+   */
+  const apiKey = `${shortid.generate()}`;
 
+  /**
+   * Store api key in file
+   */
+  fs.appendFileSync(VALID_KEYS_PATH, `${apiKey}${LINE_ENDING}`);
+
+  /**
+   * Set header and return response
+   */
+  return res.status(201).json({ apiKey });
 };
-
--- middleware.js
+++ middleware.js
@@ -1,5 +1,29 @@
-const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+const VALID_KEYS_PATH = __dirname + "/valid-keys.txt";
+const fs = require("fs");
 
 module.exports = function (req, res, next) {
+  /**
+   * Check for x-api-key in header
+   */
 
+  const apiKey = req.headers["x-api-key"];
+
+  if (!apiKey) {
+    return res
+      .status(401)
+      .json({ message: "Please provide API KEY in header" });
+  }
+
+  /**
+   * Check if API key is valid
+   */
+
+  const buffer = fs.readFileSync(VALID_KEYS_PATH);
+  const keys = buffer.toString();
+
+  if (!keys.includes(apiKey)) {
+    return res.status(401).json({ message: "API KEY is invalid" });
+  }
+
+  return next();
 };
